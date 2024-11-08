b7a84751027cb7944136946bccdf317a9083f6d9
Mahtab <mahtabazim784@gmail.com>
2024-11-08 21:41:40 +0330

    return simple value by using computed values

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,38 +1,25 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-    <h2>Refs</h2>
-    <p>{{ ninjaOne.name }} - {{ ninjaOne.age }}</p>
-    <button @click="updateNinjaOne">Update ninja one</button>
-    <h2>Reactive</h2>
-    <p>{{ ninjaTwo.name }} - {{ ninjaTwo.age }}</p>
-    <button @click="updateNinjaTwo">Update ninja two</button>
+    <p>{{ name }}</p>
   </div>
 </template>
 
 
 <script>
-import { ref, reactive } from 'vue'
+import { computed, ref } from 'vue'
 
 
 export default {
   name: 'Home',
   setup() {
-    //This ref has a value of an object, and this obj has two property.
-    const ninjaOne = ref({ name: 'mario', age: 30 })
-    //Use Reactive
-    const ninjaTwo = reactive({ name: 'luigi', age: 35 })
+    //its just return a simple value:
+   const name = computed(() => {
+    // this value assigned to name essentially when we output it (out put the name)
+    return 'mahtab'
+   })
 
-    const updateNinjaOne = () => {
-      ninjaOne.value.age = 40
-    }
-
-    //here i wrote, we dont need to use .value to acsses the value in reactive
-    const updateNinjaTwo = () => {
-      ninjaTwo.age = 45
-    }
-
-    return { ninjaOne, updateNinjaOne, ninjaTwo, updateNinjaTwo }
+   return{ name }
   }
 }
 </script>
