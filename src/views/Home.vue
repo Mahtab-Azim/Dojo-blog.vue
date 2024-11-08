2d6aa6b0c25aa89bc775ad551d650fe908894286
Mahtab <mahtabazim784@gmail.com>
2024-11-08 22:25:08 +0330

    complete the Computed Values

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,25 +1,33 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-    <p>{{ name }}</p>
+    <!-- Remind: we use v-model for Two-way Binding with data and input form -->
+    <input type="text" v-model="search">
+    <p>Search term - {{ search }}</p>
+    <div v-for="name in matchingNames" :key="name">{{ name }}</div>
   </div>
 </template>
 
 
 <script>
+
 import { computed, ref } from 'vue'
 
 
 export default {
   name: 'Home',
   setup() {
-    //its just return a simple value:
-   const name = computed(() => {
-    // this value assigned to name essentially when we output it (out put the name)
-    return 'mahtab'
-   })
+    //begin it with empty string
+    const search = ref('')
+    //create a ref and output some array of names (the array is the value of this ref (names))
+    const names = ref(['mahtab', 'matin', 'moonteen', 'melina', 'maryam', 'elham', 'mario'])
 
-   return{ name }
+    const matchingNames = computed(() => {
+      //value cause we want the value of the ref, filter method directly on names cause names is a ref object
+      return names.value.filter((name) => name.includes(search.value))
+    })
+
+   return { names, search, matchingNames }
   }
 }
 </script>
