263bb5bfb16dc2e4625b53941107706cb4a7ffa9
Mahtab <mahtabazim784@gmail.com>
2024-11-14 23:59:33 +0330

    how to use props in setup p1

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,51 +1,26 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-    <!-- Remind: we use v-model for Two-way Binding with data and input form -->
-    <input type="text" v-model="search">
-    <p>Search term - {{ search }}</p>
-    <div v-for="name in matchingNames" :key="name">{{ name }}</div>
-    <button @click="handleClick">stop watching</button>
+    <PostList :posts="posts" />
   </div>
 </template>
 
-
 <script>
-
-import { computed, ref, watch, watchEffect } from 'vue'
+import PostList from '../components/PostList.vue'
+import { ref } from 'vue'
 
 
 export default {
   name: 'Home',
+  components: { PostList },
   setup() {
  
-    const search = ref('')
-    const names = ref(['mahtab', 'matin', 'moonteen', 'melina', 'maryam', 'elham', 'mario'])
-
-    //the first argument is what we want to watch,the 2nd argument is a func wich will fire every time that search ref value changes
-    //it is watch
-   const stopWatch = watch(search, () => {
-    console.log('watch function ran')
-   })
-
-
-   //we just pass in a function we don't pass in anything to watch as the 1st argument like we did with watch
-   //this ryn initially when the component first loads or when the setup function first runs
-   const stopEffect = watchEffect(() => {
-    console.log('watchEffect function ran', search.value)
-   })
-    
-
-    const matchingNames = computed(() => {
-      return names.value.filter((name) => name.includes(search.value))
-    })
-
-    const handleClick = () => {
-      stopWatch()
-      stopEffect()
-    }
-
-   return { names, search, matchingNames, handleClick }
+   const posts = ref([
+    { title: 'welcome to the blog', body: 'Lorem ipsum', id: 1 },
+    { title: 'tap 5 css tips', body: 'lorem ispum', id: 2 },
+   ])
+  
+   return { posts }
   }
 }
 </script>
