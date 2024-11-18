bb57c7c8fce413c97a90cf5508ff9c4eddc1ea3f
Mahtab <mahtabazim784@gmail.com>
2024-11-18 23:55:49 +0330

    use different lifecycle hooks into the setup method

--- src/components/SinglePost.vue
+++ src/components/SinglePost.vue
@@ -1,10 +1,23 @@
 <template>
-    
+    <div class="post">
+        <h3>{{ post.title }}</h3>
+        <p>{{ post.body }}</p>
+        <p>{{ snippet }}</p>
+    </div>
 </template>
 
 <script>
-export default {
+import { computed } from 'vue';
 
+export default {
+    props: ['post'],
+    setup(props) {
+        const snippet = computed(() => {
+            return props.post.body.substring(0,100) + '....'
+        })
+
+        return { snippet }
+    }
 }
 </script>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-    <PostList :posts="posts" />
+    <PostList v-if="showPosts" :posts="posts" />
+    <button @click="showPosts = !showPosts">toggle posts</button>
+    <button @click="posts.pop()">delete a post</button>
   </div>
 </template>
 
@@ -16,11 +18,12 @@ export default {
   setup() {
  
    const posts = ref([
-    { title: 'welcome to the blog', body: 'Lorem ipsum', id: 1 },
-    { title: 'tap 5 css tips', body: 'lorem ispum', id: 2 },
+    { title: 'welcome to the blog', body: 'Lorem ipsum odor amet, consectetuer adipiscing elit. Sapien parturient lectus odio maximus nam semper senectus. Diam ultrices lacinia platea bibendum et condimentum leo. Risus pharetra euismod in non sodales duis pulvinar nostra. Potenti consectetur mus, venenatis maecenas senectus per fames litora. Laoreet dictum curae dolor congue libero netus cursus consequat.', id: 1 },
+    { title: 'tap 5 css tips', body: '', id: 2 },
    ])
+   const showPosts = ref(true)
   
-   return { posts }
+   return { posts, showPosts }
   }
 }
 </script>
